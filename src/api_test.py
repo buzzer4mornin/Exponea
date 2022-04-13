328aa50db17ce67d8bde3d484e2b86fd5b6d7357
Ahmadli, Aydin <aydin.ahmadli@merck.com>
2022-04-13 17:56:51 +0400

    update

--- src/api_test.py
+++ src/api_test.py
@@ -1,12 +1,29 @@
-import pytest
 from httpx import AsyncClient
-from aiohttp import ClientSession
+from fastapi.testclient import TestClient
+import pytest
 
 from api_train import app
 
+client = TestClient(app)
+
 
 @pytest.mark.anyio
-async def test_root():
-    async with AsyncClient(app=app, base_url="http://127.0.0.1") as ac:
-        response = await ac.get("/api/smart/800")
+async def test1():
+    """
+    Given enough timeout to process requests, API must return successfull response
+    """
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/api/smart/1000")
     assert response.status_code == 200
+    assert response.json()['time'] is not None
+    assert response.json()['is_successfull'] is True
+    assert response.headers.get('Content-Type') == 'application/json'
+
+
+@pytest.mark.anyio
+async def test2():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/api/smart/100")
+    assert response.status_code == 200
+    assert response.json()['time'] is None
+    assert response.json()['is_successfull'] is False
