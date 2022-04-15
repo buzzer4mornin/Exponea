e340cdf55b937447a079e664c16507e2a68c4b93
Ahmadli, Aydin <aydin.ahmadli@merck.com>
2022-04-16 02:16:05 +0400

    update

--- src/api.py
+++ src/api.py
@@ -11,7 +11,7 @@ from time import time
 app = FastAPI()
 
 
-async def send_request(connector: aiohttp.TCPConnector, timeout: int, request_num: str):
+async def send_request(connector: aiohttp.TCPConnector, timeout: int):
     try:
         async with aiohttp.ClientSession(connector=connector, timeout=timeout) as client:
             response = await client.get(
@@ -19,32 +19,32 @@ async def send_request(connector: aiohttp.TCPConnector, timeout: int, request_nu
             )
 
             json_time = await response.json(content_type=None)
-            return json_time, response.status, request_num
+            return json_time, response.status
 
     except asyncio.exceptions.TimeoutError:
-        return 'Timeout Error', 0, request_num
+        return 'Timeout Error', 0
 
     except aiohttp.client_exceptions.ClientConnectionError:
-        return 'Connection Error', 0, request_num
+        return 'Connection Error', 0
 
     except aiohttp.client_exceptions.ClientOSError:
-        return 'ClientOSError', 0, request_num
+        return 'ClientOSError', 0
 
     except asyncio.exceptions.CancelledError:
-        return 'Task got cancelled', 0, request_num
+        return 'Task got cancelled', 0
 
     except json.decoder.JSONDecodeError:
         '''
             <Response [500 Internal Server Error]>
             <Response [429 Too many requests]>
         '''
-        return 'Exponea server error [500 or 429]', 0, request_num
+        return 'Exponea server error [500 or 429]', 0
 
     except aiohttp.client_exceptions.ContentTypeError:
-        return 'JSON decode failed', 0, request_num
+        return 'JSON decode failed', 0
 
     except Exception as e:
-        return str(e), request_num
+        return str(e), 0
 
 
 @app.get("/api/smart/{ENDPOINT_TIMEOUT}")
@@ -65,10 +65,9 @@ async def api_smart(ENDPOINT_TIMEOUT: int) -> dict:
         ssl_context = ssl.create_default_context(cafile=certifi.where())
         conn_1 = aiohttp.TCPConnector(limit=100, ssl=ssl_context)
         request_1 = asyncio.create_task(
-            send_request(connector=conn_1, timeout=aiohttp.ClientTimeout(total=ENDPOINT_TIMEOUT / 1000),
-                         request_num="request_1"))
+            send_request(connector=conn_1, timeout=aiohttp.ClientTimeout(total=ENDPOINT_TIMEOUT / 1000)))
         start = time()
-        resp, status, which_request = await asyncio.wait_for(asyncio.shield(request_1), timeout=300 / 1000)
+        resp, status = await asyncio.wait_for(asyncio.shield(request_1), timeout=1000 / 1000)
         if status == 200:
             # First request is SUCCESSFUL within 300ms.
             resp["message"] = "SUCCESS"
@@ -84,21 +83,21 @@ async def api_smart(ENDPOINT_TIMEOUT: int) -> dict:
         ssl_context_3 = ssl.create_default_context(cafile=certifi.where())
         conn_2 = aiohttp.TCPConnector(limit=100, ssl=ssl_context_2)
         conn_3 = aiohttp.TCPConnector(limit=100, ssl=ssl_context_3)
+
         request_2 = asyncio.create_task(
-            send_request(connector=conn_2, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000),
-                         request_num="request_2"))
+            send_request(connector=conn_2, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000)))
         request_3 = asyncio.create_task(
-            send_request(connector=conn_3, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000),
-                         request_num="request_3"))
+            send_request(connector=conn_3, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000)))
 
         # Firing two other requests and waiting for earliest successful response.
         for request in asyncio.as_completed([request_1, request_2, request_3]):
-            earliest_resp, status, which_request = await request
+            earliest_resp, status = await request
             if status == 200:
                 earliest_resp["status"] = "SUCCESS"
                 return earliest_resp
             elif earliest_resp == "Timeout Error":  #### ADJUST AND ADD TO REPORT
                 print("ENDPOINT TIMEOUT EXCEEDED")  #### ADJUST AND ADD TO REPORT
                 return {"message": "ERROR"}
+
         # ERROR! There is no successfull response within ENDPOINT_TIMEOUT!
         return {"message": "ERROR"}
