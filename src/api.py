76d4cc2f228d215299ce05d576fe0a0d2d75accc
Ahmadli, Aydin <aydin.ahmadli@merck.com>
2022-04-16 18:39:31 +0400

    update

--- src/api.py
+++ src/api.py
@@ -57,48 +57,55 @@ async def api_smart(ENDPOINT_TIMEOUT) -> dict:
     if ENDPOINT_TIMEOUT <= 300:
         return {"message": "Endpoint timeout parameter should be above 300"}
 
-    # default is 300ms
+    # need this for properly adjusting timeout of second and third request
     time_spent = 300
 
     try:
-        # Fired first request and waiting 300ms for its response..
+        # prepare first request
         ssl_context = ssl.create_default_context(cafile=certifi.where())
         conn_1 = aiohttp.TCPConnector(limit=100, ssl=ssl_context)
 
+        # send and wait for response 300ms. Shield protects request from being cancelled by wait_for timeout
         request_1 = asyncio.create_task(
             send_request(connector=conn_1, timeout=aiohttp.ClientTimeout(total=ENDPOINT_TIMEOUT / 1000)))
         start = time()
         resp, status = await asyncio.wait_for(asyncio.shield(request_1), timeout=1000 / 1000)
+
         if status == 200:
-            # First request finished and is SUCCESSFUL within 300ms.
+            # first request finished and is SUCCESSFUL within 300ms
+            # add success message to json response and return it
             resp["message"] = "SUCCESS"
             return resp
         else:
-            # First request finished but is NOT SUCCESSFUL within 300ms.
+            # first request finished but is NOT SUCCESSFUL within 300ms
             time_spent = int((time() - start) * 1000)
             raise asyncio.exceptions.TimeoutError
 
     except asyncio.exceptions.TimeoutError:
-        # Either {300ms timeout exceeded with no response from first request} or {First request finished but is NOT SUCCESSFUL within 300ms}
+        # either {300ms timeout exceeded with no response from first request}
+        #   or   {first request finished but is NOT SUCCESSFUL within 300ms}
         ssl_context_2 = ssl.create_default_context(cafile=certifi.where())
         ssl_context_3 = ssl.create_default_context(cafile=certifi.where())
         conn_2 = aiohttp.TCPConnector(limit=100, ssl=ssl_context_2)
         conn_3 = aiohttp.TCPConnector(limit=100, ssl=ssl_context_3)
 
+        # prepare second and third request
         request_2 = asyncio.create_task(
             send_request(connector=conn_2, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000)))
         request_3 = asyncio.create_task(
             send_request(connector=conn_3, timeout=aiohttp.ClientTimeout(total=(ENDPOINT_TIMEOUT - time_spent) / 1000)))
 
-        # Firing two other requests and waiting for earliest successful response.
+        # send second and third request, and process requests greedily as they finish
         for request in asyncio.as_completed([request_1, request_2, request_3]):
             earliest_resp, status = await request
             if status == 200:
+                # received earliest successful response.
+                # add success message to json response and return it
                 earliest_resp["status"] = "SUCCESS"
                 return earliest_resp
             elif earliest_resp == "Timeout Error":
-                # ERROR! api_smart() exceeds ENDPOINT_TIMEOUT.There is no successfull response within ENDPOINT_TIMEOUT!
+                # api_smart() exceeds ENDPOINT_TIMEOUT. No successfull response within ENDPOINT_TIMEOUT!
                 return {"message": "ERROR"}
 
-        # ERROR! All requests are finished within ENDPOINT_TIMEOUT but there is no successfull response!
+        # all requests are finished within ENDPOINT_TIMEOUT but there is no successfull response!
         return {"message": "ERROR"}
