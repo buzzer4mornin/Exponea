365cc7ead508debf82bdd6d0c20d14d30c6d510b
Ahmadli, Aydin <aydin.ahmadli@merck.com>
2022-04-16 20:01:55 +0400

    update

--- src/api_test.py
+++ src/api_test.py
@@ -1,29 +1,43 @@
 from httpx import AsyncClient
-from fastapi.testclient import TestClient
 import pytest
-
 from api import app
 
-client = TestClient(app)
+
+@pytest.mark.anyio
+async def test_1():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.get(f"/api/smart/1000")
+        assert response.status_code == 200
+        assert response.headers.get('Content-Type') == 'application/json'
+        assert len(response.json()) > 0
 
 
 @pytest.mark.anyio
-async def test1():
-    """
-    Given enough timeout to process requests, API must return successfull response
-    """
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+async def test_2():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/smart/1000")
-    assert response.status_code == 200
-    assert response.json()['time'] is not None
-    assert response.json()['is_successfull'] is True
-    assert response.headers.get('Content-Type') == 'application/json'
+        assert response.status_code == 200
+        if response.json()["message"] == "SUCCESS":
+            assert len(response.json()) == 2
+            assert "time" in response.json()
+        elif response.json()["message"] == "ERROR":
+            assert len(response.json()) == 1
+            assert "time" not in response.json()
 
 
 @pytest.mark.anyio
-async def test2():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/api/smart/100")
-    assert response.status_code == 200
-    assert response.json()['time'] is None
-    assert response.json()['is_successfull'] is False
+async def test_3():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.get("/api/smart/some_string")
+        assert response.status_code == 200
+        assert len(response.json()) == 1
+        assert "Endpoint timeout parameter should be INTEGER" in response.json()["message"]
+
+
+@pytest.mark.anyio
+async def test_4():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.get("/api/smart/50")
+        assert response.status_code == 200
+        assert len(response.json()) == 1
+        assert "Endpoint timeout parameter should be above 300" in response.json()["message"]
