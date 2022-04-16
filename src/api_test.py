8c78817b43d56472d2ce0f82f4efca6fded1abd9
Ahmadli, Aydin <aydin.ahmadli@merck.com>
2022-04-16 20:03:38 +0400

    update

--- src/api_test.py
+++ src/api_test.py
@@ -17,6 +17,7 @@ async def test_2():
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/smart/1000")
         assert response.status_code == 200
+        assert response.headers.get('Content-Type') == 'application/json'
         if response.json()["message"] == "SUCCESS":
             assert len(response.json()) == 2
             assert "time" in response.json()
@@ -30,6 +31,7 @@ async def test_3():
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/smart/some_string")
         assert response.status_code == 200
+        assert response.headers.get('Content-Type') == 'application/json'
         assert len(response.json()) == 1
         assert "Endpoint timeout parameter should be INTEGER" in response.json()["message"]
 
@@ -39,5 +41,6 @@ async def test_4():
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as ac:
         response = await ac.get("/api/smart/50")
         assert response.status_code == 200
+        assert response.headers.get('Content-Type') == 'application/json'
         assert len(response.json()) == 1
         assert "Endpoint timeout parameter should be above 300" in response.json()["message"]
